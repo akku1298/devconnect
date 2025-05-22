ac0f72ffb5396f2ca90a2831b6092199b971f9d2
Akansha Srivastav <67710450+akku1298@users.noreply.github.com>
2025-05-22 15:48:07 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,47 +3,51 @@ const express = require("express");
 const app = express();
 
 
-// app.get("/user",(req, res) => {
-//     res.send({ firstname:"Akansha" , lastname :"Srivastav"});
-// })
+// app.use("/user" , (req, res, next)=>{
+//     console.log("Hi from the first");
+//     // res.send("Response 1 ");
+//     next();
+// },
+// (req, res, next)=> {
+//     console.log("Hi from the second");
+//     // res.send("Response 2");
+//     next();
+// },
+// (req, res, next)=> {
+//     console.log("Hi from the third");
+//     // res.send("Response 3");
+//     next();
+// },
+// (req, res, next)=> {
+//     console.log("Hi from the forth");
+    
+//      res.send("Response 4");
+//     next();
+// }
+// )
 
-// app.get("/user/:userid",(req, res) => {
-//     console.log(req.params);
-//     res.send({ firstname:"Akansha" , lastname :"Srivastav"});
-// })
-
-// app.get("/user", (req,res) => {
-//     console.log(req.query)
-//     res.send({ firstname:"Akansha" , lastname :"Srivastav"})
-// })
-// Express 5 mai string mai regex nhi li jayegi this is how it will work
-app.get(/^\/ab+c$/, (req, res) => {
-    res.send("Hola Amigo");
-});
-
-// app.post("/user" , (req, res) => {
-//     res.send("Db updated successfully");
-// })
-
-// app.put("/user" , (req, res) => {
-//     res.send("Db updated successfully");
-// })
-
-// app.delete("/user" , (req, res) =>{
-// res.send("Data deleted")
-// })
-
-// app.use("/test", (req, res) => {
-//     res.send("Hi form the test");
-// })
-
-// app.use("/getUser", (req, res) => {
-//     res.send("Hi from the user");
-// })
-
-// app.use("/",(req, res) => {
-//     res.send("Hi I am Akansha from the dashboard");
-// })
+app.post("/user" , [(req, res, next)=>{
+    console.log("Hi from the first");
+    // res.send("Response 1 ");
+    next();
+},
+(req, res, next)=> {
+    console.log("Hi from the second");
+    // res.send("Response 2");
+    next();
+},
+(req, res, next)=> {
+    console.log("Hi from the third");
+    // res.send("Response 3");
+    next();
+},
+(req, res, next)=> {
+    console.log("Hi from the forth");
+    
+     res.send("Response 4");
+    next();
+}]
+)
 
 app.listen(3000, () => {
     console.log("Server successfully running on 3000");
