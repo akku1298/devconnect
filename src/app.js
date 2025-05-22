1eb58701f417ea0db1add9502934df5afba48cac
Akansha Srivastav <67710450+akku1298@users.noreply.github.com>
2025-05-22 12:39:06 +0530

    Second commit with http get post and other request

--- src/app.js
+++ src/app.js
@@ -3,18 +3,47 @@ const express = require("express");
 const app = express();
 
 
+// app.get("/user",(req, res) => {
+//     res.send({ firstname:"Akansha" , lastname :"Srivastav"});
+// })
 
-app.use("/test", (req, res) => {
-    res.send("Hi form the test");
-})
+// app.get("/user/:userid",(req, res) => {
+//     console.log(req.params);
+//     res.send({ firstname:"Akansha" , lastname :"Srivastav"});
+// })
 
-app.use("/getUser", (req, res) => {
-    res.send("Hi from the user");
-})
+// app.get("/user", (req,res) => {
+//     console.log(req.query)
+//     res.send({ firstname:"Akansha" , lastname :"Srivastav"})
+// })
+// Express 5 mai string mai regex nhi li jayegi this is how it will work
+app.get(/^\/ab+c$/, (req, res) => {
+    res.send("Hola Amigo");
+});
 
-app.use("/",(req, res) => {
-    res.send("Hi I am Akansha from the dashboard");
-})
+// app.post("/user" , (req, res) => {
+//     res.send("Db updated successfully");
+// })
+
+// app.put("/user" , (req, res) => {
+//     res.send("Db updated successfully");
+// })
+
+// app.delete("/user" , (req, res) =>{
+// res.send("Data deleted")
+// })
+
+// app.use("/test", (req, res) => {
+//     res.send("Hi form the test");
+// })
+
+// app.use("/getUser", (req, res) => {
+//     res.send("Hi from the user");
+// })
+
+// app.use("/",(req, res) => {
+//     res.send("Hi I am Akansha from the dashboard");
+// })
 
 app.listen(3000, () => {
     console.log("Server successfully running on 3000");
